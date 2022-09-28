7c2047188b3f6b4ec7970579f0979eaff862d078
astel <kiyomillefeuille@gmail.com>
2022-09-28 21:32:06 +0900

    log setting

---
+++ src/main/scala/net/astail/Main.scala
@@ -0,0 +1,10 @@
+package net.astail
+
+import org.slf4j.{Logger, LoggerFactory}
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    val logger: Logger = LoggerFactory.getLogger(this.getClass)
+    logger.info("================================= start ika3 =================================")
+  }
+}
