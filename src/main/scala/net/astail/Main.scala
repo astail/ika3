3acd20e6b1f1af713cb5aa06e7a8b99716689386
astel <kiyomillefeuille@gmail.com>
2022-09-28 22:00:03 +0900

    fix start log

--- src/main/scala/net/astail/Main.scala
+++ src/main/scala/net/astail/Main.scala
@@ -4,7 +4,18 @@ import org.slf4j.{Logger, LoggerFactory}
 
 object Main {
   def main(args: Array[String]): Unit = {
+    val tag = net.astail.Git.tag
+    val rev = net.astail.Git.shortHash
     val logger: Logger = LoggerFactory.getLogger(this.getClass)
-    logger.info("================================= start ika3 =================================")
+
+    s"""
+       | ==================== start ika3 ===================
+       |
+       |    tag : $tag
+       |    rev : $rev
+       |
+       | ===================================================
+       """.stripMargin.split('\n').foreach(logger.info)
+
   }
 }
